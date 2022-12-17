3e8ec4ad10ca84387dc5430b65fca7729aacf04e
trandobulo <trandobulo@gmail.com>
2022-12-17 13:00:32 +0200

    Rename database folder

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
-import { DatabaseModule } from 'src/modules/databaseModule/database.module';
+import { DatabaseModule } from 'src/modules/database/database.module';
 import { TransactionsService } from 'src/modules/transactions/transactions.service';
 import { transactionProviders } from 'src/modules/transactions/transactions.provider';
 
